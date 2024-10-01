fd984caedbaa57be3ea8065254ebb999d0281177
almonsour13 <almonsoursalida@gmail.com>
2024-10-02 01:01:12 +0800

    update

--- app/(user)/admin/page.tsx
+++ app/(user)/admin/page.tsx
@@ -30,7 +30,7 @@ export default function Dashboard(){
               </CardDescription>
             </CardHeader>
             <CardFooter className="gap-4">
-              <Link href="/admin/scan" className="inline-flex items-center justify-center rounded-md bg-primary hover:bg-primary/90 h-8 px-4 py-2">Scan New Trees</Link>
+              <Link href="/admin/scan" className="inline-flex items-center justify-center rounded-md bg-primary text-foreground hover:bg-primary/90 h-8 px-4 py-2">Scan New Trees</Link>
             </CardFooter>
           </Card>
           <div className="flex flex-col md:flex-row items-end justify-end">
