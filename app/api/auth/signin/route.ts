55d93eeab9a8d1c597f196320efedde9bd59c4b3
almonsour13 <almonsoursalida@gmail.com>
2024-10-21 23:42:12 +0800

    Ads

--- app/api/auth/signin/route.ts
+++ app/api/auth/signin/route.ts
@@ -45,16 +45,16 @@ export async function POST(req: Request) {
           path: '/',
       });
       
-      const allowedOrigins = ['https://mangocare.netlify.app', 'https://mdd-app.vercel.app'];
-      const origin: string = req.headers.get('origin') || 'https://mangocare.netlify.app';
+      // const allowedOrigins = ['https://mangocare.netlify.app', 'https://mdd-app.vercel.app'];
+      // const origin: string = req.headers.get('origin') || 'https://mangocare.netlify.app';
 
-      if (allowedOrigins.includes(origin)) {
-          response.headers.set('Access-Control-Allow-Origin', origin);
-      }
+      // if (allowedOrigins.includes(origin)) {
+      //     response.headers.set('Access-Control-Allow-Origin', origin);
+      // }
 
-      response.headers.set('Access-Control-Allow-Credentials', 'true');
-      response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-      response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      // response.headers.set('Access-Control-Allow-Credentials', 'true');
+      // response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      // response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
       return response;
   } catch (error) {
